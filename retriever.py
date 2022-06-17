6f5c8dcffb57629c2d056ebc099850c60cbde5dc
Chris Hendrix <chendrix@emissary.io>
2022-06-17 14:42:35 -0400

    setup get and export data

--- retriever.py
+++ retriever.py
@@ -3,7 +3,13 @@ import os
 import mintapi
 import pandas as pd
 import yaml
-from datetime import date
+from datetime import date, datetime, timedelta
+
+RAW_TRANSACTIONS_FILE = 'transactions_raw.csv'
+RAW_ACCOUNTS_FILE = 'accounts_raw.csv'
+TRANSACTIONS_FILE = 'transactions.csv'
+ACCOUNTS_FILE = 'accounts.csv'
+ACCOUNT_VALUES_FILE = 'account_values.csv'
 
 
 def get_mint(email, password, wait_for_sync=False):
@@ -23,9 +29,50 @@ def get_mint(email, password, wait_for_sync=False):
 
 
 def get_transactions(mint, data_path=None):
-    # get transactions
-    trans = mint.get_transactions(include_investment=True).copy()
+    '''
+        limit=5000,
+        include_investment=False,
+        start_date=None,
+        end_date=None,
+        remove_pending=True,
+        id=0,
+    '''
+    trans = mint.get_transaction_data(limit=100000, include_investment=True)
+    trans = pd.DataFrame(trans)
 
+    if data_path is not None:
+        trans.to_csv(os.path.join(data_path, RAW_TRANSACTIONS_FILE))
+
+    return trans
+
+
+def get_accounts(mint, data_path):
+    '''
+    limit=5000,
+    '''
+    accounts = mint.get_account_data()
+    accounts = pd.DataFrame(accounts)
+    if data_path is not None:
+        accounts.to_csv(os.path.join(data_path, RAW_ACCOUNTS_FILE))
+
+    return accounts
+
+
+def load_data_from_csv(file, data_path, min_days_old=1):
+    fullname = os.path.join(data_path, file)
+    if not os.path.isfile(fullname):
+        return None
+
+    old_date = datetime.today() - timedelta(days=min_days_old)
+    file_date_modified = datetime.fromtimestamp(os.path.getmtime(fullname))
+
+    if file_date_modified < old_date:
+        return None
+
+    return pd.read_csv(fullname)
+
+
+def format_transactions(trans, out_path=None):
     # convert datetime to date
     trans['date'] = pd.to_datetime(trans['date'])
     trans['date'] = trans['date'].dt.date
@@ -41,31 +88,30 @@ def get_transactions(mint, data_path=None):
     trans.index = trans.index.rename('id')
 
     # export
-    if data_path:
-        trans.to_csv(os.join(data_path, 'transactions.csv'))
+    if out_path is not None:
+        trans.to_csv(os.path.join(out_path, TRANSACTIONS_FILE))
 
     return trans
 
 
-def get_account_data(mint, data_path):
+def format_accounts(accounts, out_path=None):
 
     # get account data
-    account_data = pd.DataFrame.from_records(mint.get_accounts())
     trouble_cols = ['possibleLinkAccounts', 'closeDateInDate']
-    account_data = account_data.drop(trouble_cols, axis=1, errors='ignore')
-    account_data['date'] = pd.to_datetime('today')
-    account_data['date'] = account_data['date'].dt.date
+    accounts = accounts.drop(trouble_cols, axis=1, errors='ignore')
+    accounts['date'] = pd.to_datetime('today')
+    accounts['date'] = accounts['date'].dt.date
 
     # accounts
-    accounts = account_data.drop_duplicates(subset=['accountName'], keep='last').reset_index(drop=True)
+    accounts = accounts.drop_duplicates(subset=['name'], keep='last').reset_index(drop=True)
 
     # account values
-    account_values = account_data[['accountName', 'value', 'date']]
+    account_values = accounts[['name', 'value', 'date']]
 
     # export
-    if data_path:
-        accounts.to_csv(os.join(data_path, 'accounts.csv'))
-        account_values.to_csv(os.join(data_path, f'account_values_{str(date.today())}.csv'))
+    if out_path is not None:
+        accounts.to_csv(os.path.join(data_path, ACCOUNTS_FILE))
+        account_values.to_csv(os.path.join(data_path, f'account_values_{str(date.today())}.csv'))
 
     return accounts, account_values
 
@@ -81,7 +127,18 @@ def get_settings(settings_yaml):
 
 if __name__ == "__main__":
     settings = get_settings('settings.yaml')
-    print(settings)
-    mint = get_mint(settings['email'], settings['password'])
-    trans = get_transactions(mint, 'data')
-    accounts, account_values = get_account_data(mint, 'data')
+    data_path = ''
+    mint = None
+
+    trans = load_data_from_csv(RAW_TRANSACTIONS_FILE, data_path)
+    if trans is None:
+        mint = get_mint(settings['email'], settings['password'])
+        trans = get_transactions(mint, data_path)
+    trans = format_transactions(trans, data_path)
+
+    accounts = load_data_from_csv(RAW_ACCOUNTS_FILE, data_path)
+    if accounts is None:
+        mint = get_mint(settings['email'], settings['password']) if mint is None else mint
+        accounts = get_accounts(mint, data_path)
+    accounts, account_values = format_accounts(accounts, data_path)
+    print(accounts.info())
